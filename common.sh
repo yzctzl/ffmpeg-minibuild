562adebf78c5c66db25d3cc8c6ba73e7addf7874
yzctzl <80516107+yzctzl@users.noreply.github.com>
2022-09-22 01:05:10 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-FFMPEG_VERSION=5.1
+FFMPEG_VERSION=5.1.s
 FFMPEG_TARBALL=ffmpeg-$FFMPEG_VERSION.tar.bz2
-FFMPEG_TARBALL_URL=http://ffmpeg.org/releases/$FFMPEG_TARBALL
+FFMPEG_TARBALL_URL=https://github.com/yzctzl/ffmpeg-minibuild/raw/master/$FFMPEG_TARBALL
 
 FFMPEG_CONFIGURE_FLAGS=(
     --disable-shared
