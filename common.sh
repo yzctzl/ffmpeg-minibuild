e93d3ceea9dd81ea615015bce9fa69768ce8868d
yzctzl <yzctzl@gmail.com>
2022-09-22 01:34:01 +0800

    upadte tarball

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FFMPEG_VERSION=5.1.s
-FFMPEG_TARBALL=ffmpeg-$FFMPEG_VERSION.tar.bz2
+FFMPEG_TARBALL=ffmpeg-$FFMPEG_VERSION.tar.xz
 FFMPEG_TARBALL_URL=https://github.com/yzctzl/ffmpeg-minibuild/raw/master/TARBALL/$FFMPEG_TARBALL
 
 FFMPEG_CONFIGURE_FLAGS=(
